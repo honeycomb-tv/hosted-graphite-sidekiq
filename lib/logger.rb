6f2101fddd6cb07c57a05fdcd6032bffd4821e5f
Anthony Dmitriyev <antstorm@gmail.com>
2016-10-02 23:51:08 +0100

    Add logger

---
+++ lib/logger.rb
@@ -0,0 +1,23 @@
+class Logger
+  class << self
+    attr_accessor :path, :level
+
+    [:debug, :warn, :info, :error].each do |level|
+      define_method level do |*args|
+        instance.send level, *args
+      end
+    end
+
+    def instance
+      @instance ||= begin
+        dir = File.dirname(path)
+        FileUtils.mkdir_p dir unless File.exists?(dir)
+        FileUtils.touch path
+
+        logger = Logger.new(path)
+        logger.level = level
+        logger
+      end
+    end
+  end
+end
